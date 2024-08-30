cb522dd5f1c99177b6fb2837ce33841af60c5fe7
Russell Mrcelo <152049439+CrumblinChipp@users.noreply.github.com>
2024-08-30 13:42:14 +0800

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Welcome {
     public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to CS 211: Object-oriented Programming!");
+        	System.out.println("Welcome to CS 211: Object-oriented Programming!");
 		System.out.print("Enter your name: ");
 		String name = scanner.nextLine();
 
