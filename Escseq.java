d55dc9117c9056b74097d9a64730774986ceeacb
Russell Mrcelo <152049439+CrumblinChipp@users.noreply.github.com>
2024-08-30 14:04:49 +0800

    Create Escseq.java

---
+++ Escseq.java
@@ -0,0 +1,20 @@
+
+class Escseq {
+    public static void main(String[] args) {
+        int ctr = 10;
+        int multiplier = 1;
+
+        while (ctr >= multiplier){
+            int product = 7 * multiplier;
+            int check = multiplier % 2;
+            if (check == 1){
+                System.out.println(product);
+            }
+            else{
+                System.out.println("\t"+product);
+            }
+            multiplier++;
+        }
+    }
+
+}
